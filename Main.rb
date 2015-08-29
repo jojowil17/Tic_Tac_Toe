4e12ae98a5c96702afff37d674a78a85e5740da3
jojowil17 <jojowil32@gmail.com>
2015-08-29 12:06:12 -0400

    Set player markers

--- Main.rb
+++ Main.rb
@@ -17,3 +17,26 @@ end
 	  
 	  board(" Welcome to Tic Tac Toe")
 	  board("The current board is:")
+
+def p1choice
+        puts " Player 1 do you want x or o"
+p1choice=gets.chomp.upcase
+        end
+
+def p2choice(p1)
+        if p1 == "X"
+                    "O"
+        else "X"      
+    end
+end
+      
+def set_players
+     p1marker = p1choice 
+	 p2marker = p2choice(p1marker)
+
+		puts "Player 1 has chosen #{p1marker}"
+		
+		puts " Player 1 is #{p1marker} and Player 2 is #{p2marker}"
+		end
+		
+	set_players		
