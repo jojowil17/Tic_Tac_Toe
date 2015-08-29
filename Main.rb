8bef7231e730eafb5bf7d90884f013c6006e1745
jojowil17 <jojowil32@gmail.com>
2015-08-29 11:05:34 -0400

    put board visulisation in a function that takes a message

---
+++ Main.rb
@@ -0,0 +1,19 @@
+
+def board(message)
+puts message
+puts """
+    | |
+   1|2|3
+  __|_|__
+    | |
+   4|5|6
+  __|_|__
+    | |
+   7|8|9
+    | |
+	
+	  """
+end
+	  
+	  board(" Welcome to Tic Tac Toe")
+	  board("The current board is:")
