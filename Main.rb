cb1095ec5a53c29b70de656d77bea9ea8441d748
jojowil17 <jojowil32@gmail.com>
2015-08-29 14:05:19 -0400

    board function takes array and updates visulization

--- Main.rb
+++ Main.rb
@@ -1,22 +1,30 @@
 
-def board(message)
+def board(message, board_array)
 puts message
+
+
+#board_positions_array[0] = "x"
+
+
 puts """
     | |
-   1|2|3
+  #{board_array[0]} |#{board_array[1]}| #{board_array[2]}  
   __|_|__
     | |
-   4|5|6
+  #{board_array[3]} |#{board_array[4]}| #{board_array[5]} 
   __|_|__
     | |
-   7|8|9
+  #{board_array[6]} |#{board_array[7]}| #{board_array[8]} 
     | |
 	
 	  """
 end
-	  
-	  board(" Welcome to Tic Tac Toe")
-	  board("The current board is:")
+	 
+board_positions_array = ["0", "1", "2", "3", "4", "5", "6", "7","8"]
+
+	 
+	 # board(" Welcome to Tic Tac Toe")
+	 board("board positions", board_positions_array)
 
 def p1choice
         puts " Player 1 do you want x or o"
@@ -31,12 +39,19 @@ def p2choice(p1)
 end
       
 def set_players
-     p1marker = p1choice 
-	 p2marker = p2choice(p1marker)
+     @p1marker = p1choice 
+	 @p2marker = p2choice(@p1marker)
 
-		puts "Player 1 has chosen #{p1marker}"
+		puts "Player 1 has chosen #{@p1marker}"
 		
-		puts " Player 1 is #{p1marker} and Player 2 is #{p2marker}"
+		puts " Player 1 is #{@p1marker} and Player 2 is #{@p2marker}"
 		end
 		
 	set_players		
+	
+puts "Player 1 where would you like to go?"
+sqrchoice=gets.to_i
+
+puts"Player 1 has chosen #{sqrchoice}"
+	
+	
