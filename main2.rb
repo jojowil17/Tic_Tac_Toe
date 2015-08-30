60b175ef4a910de7b484bb6a51706aed11285738
jojowil17 <jojowil32@gmail.com>
2015-08-30 11:15:00 -0400

    had player 1 choose x or o and player 2 gets what player1 didnt choose

--- main2.rb
+++ main2.rb
@@ -12,8 +12,26 @@ __|_|__
   | |
 	     """
 end
-board("welcome to tic tac toe")
-board("the board status is:")
+board("Welcome to tic tac toe")
+board("The board status is:")
+
+
+def p1choice
+        puts "Player 1 choose x or o"
+		gets.chomp.upcase  
+end
+
+p1marker = p1choice
+
+def p2choice(p1)
+if p1 == "X"
+            "O"
+          else "X"
+     end
+end
+p2marker = p2choice(p1marker)
+puts"player1 is #{p1marker} and player 2 is #{p2marker}"
+
 
 
 
